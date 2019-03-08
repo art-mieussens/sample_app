b8742c34b61844e2c2e706521bb2955a0cd20e5d
Art Mieussens <artmieu@gmail.com>
2019-03-08 06:50:45 +0000

    User signup redirection and flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[user_params])    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
     else
       render 'new'
     end
@@ -20,7 +21,7 @@ private
 
   def user_params
     params.require(:user)
-    .permit(:name, :email, :password, :password_confirmation)
+          .permit(:name, :email, :password, :password_confirmation)
   end
 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
-  test "invalid signup information" do
-    get signup_path 
+  test 'invalid signup information' do
+    get signup_path
+    assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
       post users_path, params: {
         user: {
