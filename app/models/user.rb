dbb3b22b2e4d5f2a3f192ec7e01c75a813477972
Art Mieussens <artmieu@gmail.com>
2019-03-10 17:29:34 +0000

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,22 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
-  validates :name,
-    presence: true,
-    length: { maximum: 50 }
-  validates :email,
-    presence: true,
-    uniqueness: { case_sensitive: false },
-    length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX }
-  validates :password,
-    presence: true,
-    length: { minimum: 6 }
-
+  validates :name,  presence: true,
+                    length: { maximum: 50 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
   before_save { self.email = email.downcase }
 
   has_secure_password
+  validates :password,  presence: true,
+                        length: { minimum: 6 }
 
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,9 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+
+  def user_is_logged_in?
+    !session[:user_id].nil?
+  end
+
 end
