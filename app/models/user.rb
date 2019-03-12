498733032104e184ab1f49c52629646b68ec67c5
Art Mieussens <artmieu@gmail.com>
2019-03-12 15:54:44 +0000

    [tutorial-9.3.1] Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
@@ -33,6 +34,7 @@ class User < ApplicationRecord
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
