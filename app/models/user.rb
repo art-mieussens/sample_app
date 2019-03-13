103b1736b16e0d82f3c5b66f69ae9f25a3071827
Art Mieussens <artmieu@gmail.com>
2019-03-13 06:47:54 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,6 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
