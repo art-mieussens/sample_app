49b7a60a0ab16b80f4da8baa5142fa4ad6638885
Art Mieussens <artmieu@gmail.com>
2019-04-08 06:45:36 +0100

    14.2.2b

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'password_resets/new'
-
   get 'password_resets/edit'
 
   root 'static_pages#home'
@@ -22,7 +21,7 @@ Rails.application.routes.draw do
     end
   end
 
-   resources :account_activations, only: [:edit]
+  resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
 end
